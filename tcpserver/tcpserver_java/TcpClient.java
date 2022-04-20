d57b73cb5e06088cca6f3107c7fcbf105be8d9fe
aki-nishikawa <akihito.n.0910@keio.jp>
2022-04-20 23:27:18 +0900

    tcp server & client writen in java

---
+++ tcpserver/tcpserver_java/TcpClient.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+
+public class TcpClient {
+    public static void main(String[] argv) throws Exception
+    {
+        try (
+            Socket socket = new Socket("localhost", 8001);
+            FileOutputStream fos = new FileOutputStream("client_recv.txt");
+            FileInputStream fis = new FileInputStream("client_send.txt");
+        ){
+            int ch;
+
+            System.out.println("client start sending data");
+            OutputStream output = socket.getOutputStream();
+            while ((ch = fis.read()) != -1)
+            {
+                output.write(ch);
+            }
+            output.write(0);
+            System.out.println("client finish sending data");
+
+            System.out.println("client start recieving data");
+            InputStream input = socket.getInputStream();
+            while ((ch = input.read()) != -1)
+            {
+                fos.write(ch);
+            }
+            System.out.println("client finish recieving data");
+
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
---
+++ tcpserver/tcpserver_java/TcpServer.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+
+public class TcpServer {
+    public static void main(String[] argv) throws Exception
+    {
+        try (
+            ServerSocket server = new ServerSocket(8001);
+            FileOutputStream fos = new FileOutputStream("server_recv.txt");
+            FileInputStream fis = new FileInputStream("server_send.txt");
+        ){
+            System.out.println("waiting access from client");
+            Socket socket = server.accept();
+            System.out.println("accessed");
+
+            int ch;
+
+            System.out.println("server start recieving data");
+            InputStream input = socket.getInputStream();
+            while ((ch = input.read()) != 0)
+            {
+                fos.write(ch);
+            }
+            System.out.println("server finish recieving data");
+
+            System.out.println("server start sending data");
+            OutputStream output = socket.getOutputStream();
+            while ((ch = fis.read()) != -1)
+            {
+                output.write(ch);
+            }
+            System.out.println("server finish sending data");
+
+            socket.close();
+            System.out.println("connection finished");
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
