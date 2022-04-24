8c8f096ee716fd887df19895d1cf422936ff947e
aki-nishikawa <akihito.n.0910@keio.jp>
2022-04-24 23:50:26 +0900

    Modoki-0.1

---
+++ modoki-0.1/ServerThread.java
@@ -0,0 +1,129 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.text.*;
+
+public class ServerThread implements Runnable
+{
+    private static final String DOCUMENT_ROOT = "/home/nishikawa/playground/webserver_creation/contents";
+    private Socket socket;
+
+    // read binary line from InputStream
+    private static String readLine(InputStream input)
+    throws Exception {
+        int ch;
+        String ret = "";
+        while ((ch = input.read()) != -1)
+        {
+            if (ch == '\r') {
+                ;
+            } else if (ch == '\n') {
+                break;
+            } else {
+                ret += (char)ch;
+            }
+        }
+        if (ch == -1) {
+            return null;
+        } else {
+            return ret;
+        }
+    }
+
+    // write binary line to OutputStream
+    private static void writeLine(OutputStream output, String str)
+    throws Exception {
+        for (char ch : str.toCharArray())
+        {
+            output.write((int)ch);
+        }
+        output.write('\r');
+        output.write('\n');
+    }
+
+    // convert time into HTTP format
+    private static String getDataStringUtc()
+    {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        DateFormat df = new SimpleDateFormat(
+            "EEE, dd MMM yyyy HH:mm:ss", Locale.US
+        );
+        df.setTimeZone(cal.getTimeZone());
+        return df.format(cal.getTime()) + " GMT";
+    }
+
+    // table of tuple File-Extension - Content-Type
+    private static final HashMap<String, String> contentTypeMap =
+        new HashMap<String, String>() {{
+            put("html", "text/html");
+            put("htm", "text/html");
+            put("txt", "text/plain");
+            put("css", "text/css");
+            put("png", "image/png");
+            put("jpg", "image/jpeg");
+            put("jpeg", "image/jpeg");
+            put("gif", "image/gif");
+        }};
+    // return Content-Type from File-Extension
+    private static String getContentType(String ext)
+    {
+        String ret = contentTypeMap.get(ext.toLowerCase());
+        if (ret == null) {
+            return "application/octet-stream";
+        } else {
+            return ret;
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        OutputStream output;
+        try {
+            // parse input
+            InputStream input = socket.getInputStream();
+            String line;
+            String path = null;
+            String ext = null;
+            while ((line = readLine(input)) != null)
+            {
+                if (line.equals("")) break;
+                if (line.startsWith("GET")) {
+                    path = line.split(" ")[1];
+                    String[] tmp = path.split("\\.");
+                    ext = tmp[tmp.length - 1];
+                }
+            }
+
+            // return output
+            output = socket.getOutputStream();
+            // response header
+            writeLine(output, "HTTP/1.1 200 OK");
+            writeLine(output, "Date: " + getDataStringUtc());
+            writeLine(output, "Server: Modoki/0.1");
+            writeLine(output, "Connection: close");
+            writeLine(output, "Content-type: " + getContentType(ext));
+            writeLine(output, "");
+            // response body
+            try (FileInputStream fis = new FileInputStream(DOCUMENT_ROOT + path)) {
+                int ch;
+                while ((ch = fis.read()) != -1)
+                {
+                    output.write(ch);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } 
+        }
+    }
+
+    ServerThread(Socket socket) {
+        this.socket = socket;
+    }
+}
